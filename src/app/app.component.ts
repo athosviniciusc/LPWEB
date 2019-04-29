7f5891a6105680e87bc2990e662055c6d0f798e9
Athos Vinicius <athos_47@hotmail.com>
2019-04-29 19:09:12 -0300

    Primeiro Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { ControlerService } from './controler.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  tela = 'inicial';
+  autor = null;
+  noticia = null;
+  
+  
+  constructor(private service: ControlerService) {
+
+  }
+  salvarPublicacao() {
+    this.tela = 'publica';
+    this.service.cadastrarAutor(this.autor,this.noticia);
+    this.autor = null;
+    this.noticia = null;
+  }
+  mudarTela(nome){
+    this.tela = nome;
+  }
+  visualizar(pessoa){
+    this.tela = 'noticia';
+    this.service.visualizarNoticia(pessoa);
+  }
+}
